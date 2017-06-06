6f6b002bdc7e8dcf7a61599447d7bf03e4911d24
tahirfazal <tahir.fazal@arbisoft.com>
2017-06-06 23:02:14 +0500

    Showing compnies data on page when click. Now looking in to selectize and understanding company api.

--- companies_house/search.py
+++ companies_house/search.py
@@ -1,7 +1,7 @@
 
-from config import COMPANIES_HOUSE_API_KEY
+from companies_house.config import COMPANIES_HOUSE_API_KEY
 import requests
-from config import _url
+from companies_house.config import _url
 
 def search_by_name(name, items_per_page = 5):
     """
