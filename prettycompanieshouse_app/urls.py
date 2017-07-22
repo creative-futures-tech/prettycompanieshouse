853ef24b7dd1dd786e1c17ecbc086100f5ac9d51
anaoaktree <anapmc.carvalho@gmail.com>
2017-07-23 00:56:21 +0100

    added pdf download

--- prettycompanieshouse_app/urls.py
+++ prettycompanieshouse_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^companies/search/$', views.get_companies_house_info, name='companies_house_info'),
+    url(r'^download/$', views.download_pdf, name='download_pdf'),
 ]
--- prettycompanieshouse_app/views.py
+++ prettycompanieshouse_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from companies_house import search
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import pdfkit
 
+pdf_config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
 # Create your views here.
 def index(request):
     return render(request, 'prettycompanieshouse_app/index.html', {})
@@ -16,3 +18,14 @@ def get_companies_house_info(request):
 
 def print_info_from_company(request, company_nr):
     pass
+
+
+def download_pdf(request):
+
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    pdf = pdfkit.from_url('http://google.com', False, configuration=pdf_config)
+    response.write(pdf)
+    return response
